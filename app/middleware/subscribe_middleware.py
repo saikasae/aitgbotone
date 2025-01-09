3381ed5a9a8294efdf045a58612ff2086f62e361
saikasae <dmolukov0@gmail.com>
2025-01-09 21:25:54 +0300

    done v1

--- app/middleware/subscribe_middleware.py
+++ app/middleware/subscribe_middleware.py
@@ -4,6 +4,7 @@ from aiogram import BaseMiddleware, Bot
 from aiogram.types import Message, CallbackQuery
 from app.keyboards import get_subs_keyboard
 
+
 class CheckSubscribeMiddleware(BaseMiddleware):
     async def __call__(
         self,
@@ -12,21 +13,23 @@ class CheckSubscribeMiddleware(BaseMiddleware):
         data: Dict[str, Any],
     ):
         user = message.from_user
-        group = os.getenv('GROUP')
+        group = os.getenv("GROUP")
         try:
             user_subscription_status = await message.bot.get_chat_member(
-                chat_id=group,
-                user_id=user.id
+                chat_id=group, user_id=user.id,
             )
 
             status = str(user_subscription_status).split()[0][8:-1]
-            if status == 'left':
-                await message.answer('👋 Для того, чтобы пользоваться ботом, вам необходимо подписаться на наш канал.\n'
-                         'После подписки, нажмите на соответствующую кнопку.', reply_markup=get_subs_keyboard()
+            if status == "left":
+                await message.answer(
+                    "👋 Для того, чтобы пользоваться ботом, вам необходимо подписаться на наш канал\n"
+                    "После подписки, нажмите на соответствующую кнопку",
+                    reply_markup=get_subs_keyboard(),
                 )
                 return
+
             return await handler(message, data)
         except Exception as e:
-            print(f'Произошла ошибка: {e}')
-            await message.answer("Произошла ошибка! Попробуйте снова.")
+            print(f"Произошла ошибка: {e}")
+            await message.answer("Произошла ошибка! Попробуйте снова")
             return
